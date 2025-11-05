76db0e52ade392a98a41254c8a1b6623e238ff4b
Rainbow P <pablososek@gmail.com>
2025-11-05 16:07:56 +0100

    Finish project
    Removed useless imports
    
    Calculate overall price and output message at bottom
    
    Fixed additionalEq not updating correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css'
-import szary from './assets/szary.png'
-import czerwony from './assets/czerwony.png'
-import granatowy from './assets/granatowy.png'
-import zielony from './assets/zielony.png'
-import zolty from './assets/zolty.png'
 
 function App() {
   const lakierSelected = useRef(null);
-  const rimsSelected = useRef(null);
+  const rimsAluminiumSelected = useRef(null)
   const parkingSensorsCheck = useRef(null);
   const climatronicCheck = useRef(null);
   const navigationCheck = useRef(null);
@@ -27,27 +22,64 @@ function App() {
     dodatkowe_wyposazenia: [false, false, false]
     //0 - czujniki parkowania 1 - climatronic 2 - nawigacja
   })
-
+  
+  const [message, setMessage] = useState("Cena bazowa: 75000 PLN \n\nRAZEM: 75000 PLN")
+  
+  function calculatePrice() {
+    var price = CENA_BAZOWA;
+    
+    if(auto.lakier != "szary") price += CENA_LAKIER
+    if(auto.felgi == "aluminiowe") price += CENA_FELGI
+    if(auto.dodatkowe_wyposazenia[0]) price += CENA_CZUJNIKI
+    if(auto.dodatkowe_wyposazenia[1]) price += CENA_CLIMATRONIC
+    if(auto.dodatkowe_wyposazenia[2]) price += CENA_NAWIGACJA
+    
+    return price;
+  }
+  
+  function updatePrices() {
+    var messageUpdated = `Cena bazowa: ${CENA_BAZOWA} PLN`;
+    
+    if (auto.lakier != "szary") messageUpdated += `\nLakier: ${CENA_LAKIER} PLN`;
+    if (auto.felgi == "aluminiowe") messageUpdated += `\nFelgi aluminiowe: ${CENA_FELGI} PLN`;
+    if (auto.dodatkowe_wyposazenia[0]) messageUpdated += `\nCzujniki parkowania: ${CENA_CZUJNIKI} PLN`;
+    if (auto.dodatkowe_wyposazenia[1]) messageUpdated += `\nClimatronic: ${CENA_CLIMATRONIC} PLN`;
+    if (auto.dodatkowe_wyposazenia[2]) messageUpdated += `\nNawigacja: ${CENA_NAWIGACJA} PLN`;
+    
+    messageUpdated += `\n\nRAZEM: ${calculatePrice()} PLN`;
+    
+    setMessage(messageUpdated);   
+  }
+  
   function handleSelect() {
     setAuto(prevAuto => ({
       ...prevAuto,
       lakier: lakierSelected.current.value
     }))
   }
-
+  
   function handleRims() {
+    var value = "stalowe"
+    if(rimsAluminiumSelected.current.checked)
+      value = "aluminiowe"
+    
     setAuto(prevAuto => ({
       ...prevAuto,
-      felgi: rimsSelected.current.id
+      felgi: value
     }))
   }
-
+  
+  
   function handleEquipment() {
     setAuto(prevAuto => ({
       ...prevAuto,
-      dodatkowe_wyposazenie: [parkingSensorsCheck.current.checked, climatronicCheck.current.checked, navigationCheck.current.checked]
+      dodatkowe_wyposazenia: [parkingSensorsCheck.current.checked, climatronicCheck.current.checked, navigationCheck.current.checked]
     }))
   }
+  
+  useEffect(() => {
+    updatePrices()
+  }, [auto])
 
   return (
     <>
@@ -57,7 +89,7 @@ function App() {
         
         <h3 className='otherText'>Wybierz kolor lakieru</h3>
         <div className='selectLakier'>
-          <select ref={lakierSelected} className='border-0' defaultValue={szary} onChange={handleSelect}>
+          <select ref={lakierSelected} className='border-0' defaultValue="szary" onChange={handleSelect}>
             <option value="szary">szary</option>
             <option value="czerwony">czerwony</option>
             <option value="zielony">zielony</option>
@@ -71,12 +103,12 @@ function App() {
         <h3 className='otherText'>Felgi</h3>
         <div className='rimsSelect'>
           <div className='leftRim'>
-            <input ref={rimsSelected} className="form-check-input" type='radio' name='rims' id='stalowe' defaultChecked onChange={handleRims} />
-            <label htmlFor="stalowe">Stalowe</label>
+            <input  className="form-check-input" type='radio' name='rims' id='rimsSteel' value="stalowe" defaultChecked onChange={handleRims} />
+            <label htmlFor="rimsSteel">Stalowe</label>
           </div>
           <div className='rightRim'>
-            <input ref={rimsSelected} className="form-check-input" type='radio' name='rims' id='aluminiowe' onChange={handleRims} />
-            <label htmlFor="aluminiowe">Aluminiowe</label>
+            <input ref={rimsAluminiumSelected} className="form-check-input" type='radio' name='rims' id='rimsAluminium' value="aluminiowe" onChange={handleRims} />
+            <label htmlFor="rimsAluminium">Aluminiowe</label>
           </div>
         </div>
 
@@ -95,7 +127,7 @@ function App() {
         <hr/>
 
         <h3 className='otherText'>Wycena</h3>
-        <p className='priceText'>Cena bazowa: 75000 PLN <br/><br/>RAZEM: 75000 PLN</p>
+        <p className='priceText'><pre>{message}</pre></p>
       </div>
     </>
   )
